10584c025bd4df3766c0ab1674b26308a97c07b8
mnguemnin <137334019+mnguemnin@users.noreply.github.com>
2023-09-10 22:40:41 +0200

    adding backend models

---
+++ backend/src/Models/food.model.js
@@ -0,0 +1,25 @@
+import {model, Schema} from 'mongoose';
+
+export const FoodSchema= new Schema(
+    {
+        name: {type:String, required:true},
+        price:{type:Number, required:true},
+        tags: {type:[String]},
+        favorite:{type:Boolean, default:false},
+        stars:{type:Number, default:3},
+        imageUrl:{type: String, required:true},
+        origins:{type:[String], requirea:true},
+        cookTime: {type:String, required: true},
+    },
+    {
+        timestamps:true,
+        toJSON:{
+            virtuals:true,
+        },
+        toObject:{
+            virtuals:true,
+        },
+    }
+)
+
+export const FoodModel=model('food',FoodSchema);
