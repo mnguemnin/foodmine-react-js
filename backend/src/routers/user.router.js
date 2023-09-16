d51b855faf571c6ca44d54129f9dde29e097cbb8
mnguemnin <137334019+mnguemnin@users.noreply.github.com>
2023-09-17 01:09:51 +0200

    adding update user api

---
+++ backend/src/routers/user.router.js
@@ -0,0 +1,86 @@
+import { Router } from "express";
+import { sample_users } from "../data.js";
+import jwt from 'jsonwebtoken';
+import { BAD_REQUEST } from "../constants/httpStatus.js";
+import handler from 'express-async-handler';
+import { UserModel } from "../Models/user.model.js";
+import bcrypt from 'bcryptjs';
+import authMid from "../middleware/auth.mid.js";
+const PASSWORD_HASH_SALT_ROUNDS=10;
+
+
+const router=Router();
+
+router.post('/login',handler(async (req,res)=>{
+    const {email, password }=req.body;
+        const user=await UserModel.findOne({email});
+        if(user&& (await bcrypt.compare(password, user.password))){
+           res.send(generateTokenResponse(user));
+           return;
+        }
+        res.status(BAD_REQUEST).send('Username or password is invalid');
+}));
+
+router.post(
+    '/register',
+    handler(async(req,res)=>{
+        const {name, email, password, address}=req.body;
+
+        const user = await UserModel.findOne({email});
+
+        if(user){
+            res.status(BAD_REQUEST).send('User already exist, please login!');
+            return;
+        }
+
+        const hashedPassword= await bcrypt.hash(
+            password,
+            PASSWORD_HASH_SALT_ROUNDS
+        );
+
+        const newUser={
+            name,
+            email: email.toLowerCase(),
+            password:hashedPassword,
+            address,
+        };
+
+        const result=await UserModel.create(newUser);
+        res.send(generateTokenReponse(result));   
+    })
+);
+
+const generateTokenResponse=user=>{
+    const token=jwt.sign(
+        {
+            id: user.id,
+            email:user.email,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '30d'
+        }
+    );
+
+    router.put('/updateProfile', authMid, handler(async(req, res)=>{
+        const {name, address}=req.body;
+        const user= await UserModel.findByIdAndUpdate(
+            req.user.id,
+            {name, address},
+            {new: true}
+        );
+        res.send(generateTokenResponse(user));
+    }) )
+
+    return{
+        id: user.id,
+        email:user.email,
+        name:user.name,
+        address:user.address,
+        isAdmin:user.isAdmin,
+        token,
+    };
+}
+
+export default router;
